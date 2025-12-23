025e037f69a992f0b0aa7efa66893e5c87777e8e
Sankalp Garg <sankalp@optexity.com>
2025-12-23 06:42:52 -0800

    remove patches

--- update_submodule.sh
+++ update_submodule.sh
@@ -7,12 +7,3 @@ echo "Updating submodules and applying patches"
 
 git submodule sync
 git submodule update --init --recursive
-
-PATCH="../../patches/browser-use.patch"
-SUBMODULE="external/browser-use"
-
-if git -C "$SUBMODULE" apply --reverse --check "$PATCH" 2>/dev/null; then
-    echo "Patch already applied, skipping."
-else
-    git -C "$SUBMODULE" apply "$PATCH"
-fi
